4c20d0b5f0a0637f95ba5c00495d5c7415843b20
Гуцалюк Юрій <hutsaluck@gmail.com>
2022-06-03 09:34:33 +0300

    Testing functionality

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+//        $schedule->command('test')->hourly();
         // $schedule->command('inspire')->hourly();
     }
 
