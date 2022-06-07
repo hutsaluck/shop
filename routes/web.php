2693fe94c1554fc2e97ea86479b8647e4f278a3c
Гуцалюк Юрій <hutsaluck@gmail.com>
2022-06-07 05:14:27 +0300

    Change to API

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,4 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-Auth::routes();
 
-Route::get('/', [App\Http\Controllers\PageController::class, 'index'])->name('index');
-
-Route::resource('catalog', App\Http\Controllers\CatalogController::class)->parameters([
-    'catalog' => 'slug'
-]);
-
-Route::prefix('cart')->group(function()
-{
-    Route::get('/',  [App\Http\Controllers\CartController::class, 'index'])->name('cart.index');
-    Route::get('add/{product_id}',  [App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
-    Route::patch('update',  [App\Http\Controllers\CartController::class, 'update'])->name('cart.update');
-    Route::get('drop',  [App\Http\Controllers\CartController::class, 'drop'])->name('cart.drop');
-    Route::get('destroy',  [App\Http\Controllers\CartController::class, 'destroy'])->name('cart.destroy');
-    Route::get('checkout',  [App\Http\Controllers\CartController::class, 'checkout'])->name('cart.checkout');
-    Route::get('success/{orderId}',  [App\Http\Controllers\CartController::class, 'success'])->name('cart.success');
-});
-
-Route::resource('order', 'OrderController', ['only' => ['store', 'update', 'destroy', 'show']]);
